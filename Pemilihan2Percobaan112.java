d951ff280eb512547de5a04dc6db22a1241455be
Josephinnnes <144303247+Josephinnnes@users.noreply.github.com>
2023-10-08 00:25:57 +0700

    Add files via upload

--- Pemilihan2Percobaan112.java
+++ Pemilihan2Percobaan112.java
@@ -6,14 +6,10 @@ public class Pemilihan2Percobaan112 {
         System.out.println("Masukkan Tahun : ");
         tahun=input12.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
-                System.out.println("Tahun Kabisat");
-            }else{
-                System.out.println("Bukan Tahun Kabisat");
-            }
-            }else{
-                System.out.println("Bukan Tahun Kabisat");
-            }
+        if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
+            System.out.println("Tahun Kabisat");
+        }else{
+            System.out.println("Bukan Tahun Kabisat");
         }
     }
+}
